2acf1b97efce3de352df39b994bc1c52a0c3da88
Valérie Pirenne <vp@aproove.com>
2020-04-20 22:06:47 +0200

    Add view and action button

--- addons/my_module/models/project.py
+++ addons/my_module/models/project.py
@@ -7,6 +7,15 @@ class Project(models.Model):
     # _order = 'sequence'
 
     name = fields.Char(string="Name", index=True)
-    date_from = fields.Datetime(string="Start Date")
+    date_from = fields.Datetime(string="Start Date", required=True)
     date_to = fields.Datetime(string="End Date")
     user_id = fields.Many2one(comodel_name="res.users", string="Owner")
+    state = fields.Selection(
+        selection=[("progress", "In Progress"), ("approved", "Approved"), ("rejected", "Rejected")],
+        string="Status",
+        default="progress",
+    )
+
+    def action_approve(self):
+        for project in self:
+            project.state = "approved"
