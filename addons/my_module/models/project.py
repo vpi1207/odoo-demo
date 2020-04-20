1afaf6d58ffd303768697de50540fff7dc18c768
Valérie Pirenne <vp@aproove.com>
2020-04-20 22:43:54 +0200

    Add tags

--- addons/my_module/models/project.py
+++ addons/my_module/models/project.py
@@ -4,7 +4,7 @@ from odoo import _, api, fields, models, tools
 class Project(models.Model):
     _name = "project"
     _description = "The project of my module"
-    # _order = 'sequence'
+    _order = "date_from"
 
     name = fields.Char(string="Name", index=True)
     date_from = fields.Datetime(string="Start Date", required=True)
@@ -15,7 +15,18 @@ class Project(models.Model):
         string="Status",
         default="progress",
     )
+    tag_ids = fields.Many2many("project.tag", string="Custom Tags",)
 
     def action_approve(self):
         for project in self:
             project.state = "approved"
+
+
+class ProjectTag(models.Model):
+    _name = "project.tag"
+    _description = "The tags"
+
+    name = fields.Char(required=True)
+    color = fields.Integer(string="Color Index")
+
+    _sql_constraints = [("name_uniq", "unique (name)", "Tag name already exists!")]
